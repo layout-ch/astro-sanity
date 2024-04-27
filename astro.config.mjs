44102a15a67823c14965c3eed48958f3c75f0035
layout-ch <webmaster@layout.ch>
2024-04-27 16:58:58 +0200

    x

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    i18n: {
+        defaultLocale: "fr",
+        locales: ["fr", "de", "en"],
+        routing: {
+            prefixDefaultLocale: true
+        }
+    }
+});
