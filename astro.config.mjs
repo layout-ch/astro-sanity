4901a3261c4afdcff3d38abab3c303939b5a669e
layout-ch <webmaster@layout.ch>
2024-04-28 14:31:42 +0200

    x

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
 
+import liciousI18n from "@astrolicious/i18n";
+
 // https://astro.build/config
 export default defineConfig({
-    i18n: {
-        defaultLocale: "fr",
-        locales: ["fr", "de", "en"],
-        routing: {
-            prefixDefaultLocale: true,
-            redirectToDefaultLocale: false
-        }
-    },
-    output: "hybrid", adapter: vercel()
+  i18n: {
+    defaultLocale: "fr",
+    locales: ["fr", "de", "en"],
+    routing: {
+      prefixDefaultLocale: true,
+      redirectToDefaultLocale: false
+    }
+  },
+  output: "hybrid",
+  adapter: vercel(),
+  integrations: [liciousI18n()]
 });
