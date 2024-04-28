ab391be286d3d2e7d1bb7f4486b05166d5170ee8
layout-ch <webmaster@layout.ch>
2024-04-28 21:51:39 +0200

    Update language.ts

--- api/language.ts
+++ api/language.ts
@@ -1,14 +1,19 @@
-import type { VercelResponse, VercelRequest } from '@vercel/node'
+import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
     const acceptedLanguages = req.headers['accept-language'];
+
     if (acceptedLanguages) {
-        if (acceptedLanguages.includes('de')) {
-            res.redirect(301, '/de/');
-        } else if (acceptedLanguages.includes('fr')) {
-            res.redirect(301, '/fr/');
-        } else if (acceptedLanguages.includes('en')) {
-            res.redirect(301, '/en/');
+        if (typeof acceptedLanguages === 'string') {
+            if (acceptedLanguages.includes('de')) {
+                res.redirect(301, '/de/');
+            } else if (acceptedLanguages.includes('fr')) {
+                res.redirect(301, '/fr/');
+            } else if (acceptedLanguages.includes('en')) {
+                res.redirect(301, '/en/');
+            } else {
+                res.redirect(301, '/en/');
+            }
         } else {
             res.redirect(301, '/en/');
         }
