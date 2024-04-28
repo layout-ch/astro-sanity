a12972ea2a8b1c764f8f75537284fa2aa084c7ef
layout-ch <webmaster@layout.ch>
2024-04-28 23:05:06 +0200

    x

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
-// import liciousI18n from "@astrolicious/i18n";
+import liciousI18n from "@astrolicious/i18n";
 
 import { sanityIntegration } from "@sanity/astro";
 import react from "@astrojs/react";
@@ -11,18 +11,18 @@ export default defineConfig({
   output: "hybrid",
   adapter: vercel(),
   integrations: [
-    //   liciousI18n({
-    //   defaultLocale: "fr",
-    //   locales: ["fr", "de", "en"],
-    //   strategy: "prefix",
-    //   sitemap: true,
-    //   pages: {
-    //     "a-propos": {
-    //       de: "ueber-uns",
-    //       en: "about-us"
-    //     }
-    //   }
-    // })
+    liciousI18n({
+      defaultLocale: "fr",
+      locales: ["fr", "de", "en"],
+      strategy: "prefix",
+      sitemap: true,
+      pages: {
+        "a-propos": {
+          de: "ueber-uns",
+          en: "about-us"
+        }
+      }
+    })
     , sanityIntegration(
       {
         projectId: "t9cmnozc",
