80d18946a880fd56e44f8c10cec87120f15fd683
layout-ch <webmaster@layout.ch>
2024-04-28 21:56:45 +0200

    Update language.ts

--- api/language.ts
+++ api/language.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
-export default function handler(req: VercelRequest, res: VercelResponse) {
+export default function handler(req: VercelRequest, res: VercelResponse): void {
     const acceptedLanguages = req.headers['accept-language'];
 
     if (acceptedLanguages) {
