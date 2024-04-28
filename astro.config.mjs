ee7ea631b791d828f6d19c6a55caa4416fdd8501
layout-ch <webmaster@layout.ch>
2024-04-28 23:00:32 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import vercel from "@astrojs/vercel/serverless";
-import liciousI18n from "@astrolicious/i18n";
+// import liciousI18n from "@astrolicious/i18n";
 
 import { sanityIntegration } from "@sanity/astro";
 import react from "@astrojs/react";
@@ -10,23 +10,25 @@ export default defineConfig({
   site: "https://test.com",
   output: "hybrid",
   adapter: vercel(),
-  integrations: [liciousI18n({
-    defaultLocale: "fr",
-    locales: ["fr", "de", "en"],
-    strategy: "prefix",
-    sitemap: true,
-    pages: {
-      "a-propos": {
-        de: "ueber-uns",
-        en: "about-us"
+  integrations: [
+    //   liciousI18n({
+    //   defaultLocale: "fr",
+    //   locales: ["fr", "de", "en"],
+    //   strategy: "prefix",
+    //   sitemap: true,
+    //   pages: {
+    //     "a-propos": {
+    //       de: "ueber-uns",
+    //       en: "about-us"
+    //     }
+    //   }
+    // })
+    , sanityIntegration(
+      {
+        projectId: "t9cmnozc",
+        dataset: "production",
+        useCdn: false,
+        studioBasePath: "/admin",
       }
-    }
-  }), sanityIntegration(
-    {
-      projectId: "t9cmnozc",
-      dataset: "production",
-      useCdn: false,
-      studioBasePath: "/admin",
-    }
-  ), react()]
+    ), react()]
 });
