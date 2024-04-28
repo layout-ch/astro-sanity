d7cd6b34e42ce8d7bea32526d545df1062545da7
layout-ch <webmaster@layout.ch>
2024-04-28 22:04:52 +0200

    x

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 import liciousI18n from "@astrolicious/i18n";
 
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://test.com",
-  output: "hybrid",
+  output: "static",
   adapter: vercel(),
   integrations: [liciousI18n({
     defaultLocale: "fr",
